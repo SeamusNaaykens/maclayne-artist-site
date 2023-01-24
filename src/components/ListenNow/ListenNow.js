a1ff98b7c4d60429a4948c4db97fdf80eb4fb077
Seamus Naaykens <snaaykens@gmail.com>
2023-01-24 16:38:58 -0700

    styled the entire homepage with responsive styling at the tablet and desktop levels

--- src/components/ListenNow/ListenNow.js
+++ src/components/ListenNow/ListenNow.js
@@ -4,33 +4,37 @@ function ListenNow() {
     return (
         <section className='listen-now'>
             <h2 className='listen-now__heading'>Listen Now.</h2>
-            <iframe
-                className='listen-now__iframe'
-                width="100%"
-                scrolling="no"
-                frameBorder="no"
-                allow="autoplay"
-                title='If You Let Go'
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1377268738&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
-            </iframe>
-            <div className='listen-now__iframe-container'>
-                <a
-                    className='listen-now__iframe-link'
-                    href="https://soundcloud.com/maclaynes" title="Maclayne" target="_blank" rel="noreferrer">Maclayne</a> · <a className='listen-now__iframe-link' href="https://soundcloud.com/maclaynes/if-you-let-go-demo" title="If You Let Go Demo" target="_blank" rel="noreferrer">If You Let Go Demo</a>
-            </div>
-            <iframe
-                className='listen-now__iframe'
-                width="100%"
-                scrolling="no"
-                frameBorder="no"
-                allow="autoplay"
-                title='Ometepe Song'
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1377271996&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
-            </iframe>
-            <div className='listen-now__iframe-container--2'>
-                <a
-                    className='listen-now__iframe-link'
-                    href="https://soundcloud.com/maclaynes" title="Maclayne" target="_blank" rel="noreferrer" >Maclayne</a> · <a className='listen-now__iframe-link' href="https://soundcloud.com/maclaynes/the-ometepe-song" title="The Ometepe Song Demo" target="_blank" rel="noreferrer">The Ometepe Song Demo</a>
+            <div className='listen-now__container'>
+                <div>
+                    <iframe
+                        className='listen-now__iframe'
+                        scrolling="no"
+                        frameBorder="no"
+                        allow="autoplay"
+                        title='If You Let Go'
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1377268738&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                    <div className='listen-now__iframe-container'>
+                        <a
+                            className='listen-now__iframe-link'
+                            href="https://soundcloud.com/maclaynes" title="Maclayne" target="_blank" rel="noreferrer">Maclayne</a> · <a className='listen-now__iframe-link' href="https://soundcloud.com/maclaynes/if-you-let-go-demo" title="If You Let Go Demo" target="_blank" rel="noreferrer">If You Let Go Demo</a>
+                    </div>
+                </div>
+                <div>
+                    <iframe
+                        className='listen-now__iframe'
+                        scrolling="no"
+                        frameBorder="no"
+                        allow="autoplay"
+                        title='Ometepe Song'
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1377271996&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                    <div className='listen-now__iframe-container--2'>
+                        <a
+                            className='listen-now__iframe-link'
+                            href="https://soundcloud.com/maclaynes" title="Maclayne" target="_blank" rel="noreferrer" >Maclayne</a> · <a className='listen-now__iframe-link' href="https://soundcloud.com/maclaynes/the-ometepe-song" title="The Ometepe Song Demo" target="_blank" rel="noreferrer">The Ometepe Song Demo</a>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- src/components/LiveSessions/LiveSessions.js
+++ src/components/LiveSessions/LiveSessions.js
@@ -4,20 +4,22 @@ function LiveSessions() {
     return (
         <section className='live-sessions'>
             <h2 className='live-sessions__heading'>Live sessions.</h2>
-            <iframe
-                className='live-sessions__video'
-                src="https://www.youtube.com/embed/ec-wAEupTS4"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-            </iframe>
-            <iframe
-                className='live-sessions__video'
-                src="https://www.youtube.com/embed/HKgVwEjVf4w"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-            </iframe>
+            <div className='live-sessions__container'>
+                <iframe
+                    className='live-sessions__video'
+                    src="https://www.youtube.com/embed/ec-wAEupTS4"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+                </iframe>
+                <iframe
+                    className='live-sessions__video'
+                    src="https://www.youtube.com/embed/HKgVwEjVf4w"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+                </iframe>
+            </div>
         </section>
     )
 }
